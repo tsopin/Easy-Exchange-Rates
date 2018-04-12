fc7c3b47fab5756bdf31e04179e3efa7e86892b8
tsopin <timofei.sopin@gmail.com>
2018-04-12 09:27:55 -0400

    Initial Commit

---
+++ Easy Exchange Rates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easy Exchange Rates
+//
+//  Created by Timofei Sopin on 2018-04-12.
+//  Copyright © 2018 Timofei Sopin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
