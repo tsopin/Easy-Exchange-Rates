4b607b8e1557a91fe67d676af73b981eb2769517
tsopin <timofei.sopin@gmail.com>
2018-04-12 12:02:03 -0400

    Initial commit

---
+++ Easy Exchange Rates/Controller/CurrencyVC.swift
@@ -0,0 +1,44 @@
+//
+//  CurrencyVC.swift
+//  Easy Exchange Rates
+//
+//  Created by Timofei Sopin on 2018-04-12.
+//  Copyright © 2018 Timofei Sopin. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyVC: UIViewController {
+  
+  
+  @IBOutlet weak var oneUnitLabel: UILabel!
+  
+  @IBOutlet weak var currentRateLabel: UILabel!
+  
+  @IBOutlet weak var compareToLabel: UILabel!
+  
+  @IBOutlet weak var chartView: UIView!
+  
+  
+  @IBOutlet weak var currencyTableView: UITableView!
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+  @IBAction func compareToButton(_ sender: Any) {
+  }
+  
+
+}
+
---
+++ Easy Exchange Rates/View/CurrencyCell.swift
@@ -0,0 +1,24 @@
+//
+//  CurrencyCell.swift
+//  Easy Exchange Rates
+//
+//  Created by Timofei Sopin on 2018-04-12.
+//  Copyright © 2018 Timofei Sopin. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
